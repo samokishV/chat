a2de558d9741c79d91e049c3bea6fc22bb9cef3a
Viktoriia Samokysh <viktoriia.samokysh@codeit.com.ua>
2019-07-01 17:52:26 +0300

    Add login

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const expressHbs = require("express-handlebars");
 const hbs = require("hbs");
 const bodyParser = require("body-parser");
@@ -7,6 +9,15 @@ const {check, validationResult} = require('express-validator');
 const User = require("./user.js");
 
 const app = express();
+
+app.use(cookieParser());
+// Use the session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: true
+}));
 
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 
@@ -20,11 +31,57 @@ app.engine("hbs", expressHbs({
 ));
 
 app.get(["/", "/chat"], function(request, response){
-    response.send("<h2>Chat page</h2>");
+    response.send("<h2>Chat page</h2>" +
+        "user_id" + request.session.user_id);
 });
 
 app.get("/login", function(request, response){
-    response.render("login.hbs");
+    response.render("login.hbs", {
+        title: "Log in"
+    });
+});
+
+app.post("/login", urlencodedParser, [
+        check('login')
+            .trim()
+            .escape()
+            .custom(login  => {
+                return User.loginExists(login).then(user => {
+                    if(!user) return Promise.reject();
+                });
+            })
+            .withMessage('Login doesn\'t exists'),
+
+        check('password')
+            .custom((password, {req}) => {
+                let login = req.body.login;
+                return User.passwordExists(login, password).then(user => {
+                    if(!user) {
+                        return Promise.reject();
+                    }
+                });
+            })
+            .withMessage('Wrong password'),
+    ],
+    function(request, response){
+        const errors = validationResult(request);
+
+        let login = request.body.login;
+        let password = request.body.password;
+
+        if (errors.isEmpty()) {
+            User.findByLogin(login).then(user => {
+                request.session.user_id = user.id;
+                response.redirect("/chat");
+            })
+        } else {
+            response.render("login.hbs", {
+                title: "Log in",
+                login: login,
+                password: password,
+                errors: errors.array()
+            });
+        }
 });
 
 app.get("/register", function(request, response){
@@ -34,30 +91,35 @@ app.get("/register", function(request, response){
 });
 
 app.post("/register", urlencodedParser, [
-        check('login').not().isEmpty().trim().escape()
-            .custom((value, { req }) => {
-                return new Promise((resolve, reject) => {
-                    User.findOne({where: {login: value}}).then(user=>{
-                        if(!user) resolve();
-                        else reject();
-                    }).catch(err=>console.log(err));
+        check('login')
+            .trim()
+            .escape()
+            .not().isEmpty()
+            .withMessage('Login shouldn\'t be empty')
+            .custom(login =>  {
+                return User.loginExists(login).then(user => {
+                    if(user) return Promise.reject();
                 });
-            }).withMessage('Login is already taken.'),
-        check('password').not().isEmpty().isLength({min: 5}).withMessage('Your password must be at least 5 characters'),
+            }).withMessage('Login already registered'),
+
+        check('password')
+            .isLength({min: 5})
+            .withMessage('Your password must be at least 5 characters'),
     ],
     function(request, response){
         const errors = validationResult(request);
-        console.log(errors);
 
-        var login = request.body.login;
-        var password = request.body.password;
+        let login = request.body.login;
+        let password = request.body.password;
 
         if (errors.isEmpty()) {
             User.create({
                 login: login,
                 password: password
-            }).then(
-                response.redirect("/chat")
+            }).then(user => {
+                    request.session.user_id = user.id;
+                    response.redirect("/chat")
+                }
             ).catch(err=>console.log(err));
         } else {
             response.render("registration.hbs", {
--- user.js
+++ user.js
@@ -37,5 +37,48 @@ User.beforeCreate((user, options) => {
         });
 });
 
+/**
+ * @param {string} login
+ * @returns Promise
+ */
+User.loginExists = function(login) {
+    return User.findOne({where: {login: login}}).then(user=> {
+        if(user) return true;
+    });
+};
+
+/**
+ * @param {string} login
+ * @param {string} password
+ * @returns Promise
+ */
+User.passwordExists = async function(login, password) {
+    let user = await User.findByLogin(login);
+    if(user) {
+        return User.passwordsIsEqual(password, user.password);
+    }
+};
+
+/**
+ * @param {string} password1
+ * @param {string} password2
+ * @returns Promise
+ */
+User.passwordsIsEqual = function(password1, password2) {
+    return bcrypt.compare(password1, password2).then(res => {
+        return res;
+    });
+};
+
+/**
+ * @param {string} login
+ * @returns Promise
+ */
+User.findByLogin = function(login) {
+    return User.findOne({where: {login: login}}).then(user=> {
+        return user;
+    });
+};
+
 module.exports = User;
 
