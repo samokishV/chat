d235fa9c584e34b9ebc0a7254bce3cb5367a7a1c
Viktoriia Samokysh <viktoriia.samokysh@codeit.com.ua>
2019-07-17 11:54:28 +0300

    Add route file

--- index.js
+++ index.js
@@ -1,90 +1,18 @@
+const config = require('config');
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const redis = require('redis');
-const RedisStore = require('connect-redis')(session);
-
-const client = redis.createClient();
-
-const expressHbs = require('express-handlebars');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const config = require('config');
-const equalHelper = require('./helpers/equalHelper');
-const auth = require('./middleware/auth');
-const authController = require('./controllers/authController');
-const chatController = require('./controllers/chatController');
-const messageController = require('./controllers/messageController');
-const validate = require('./requests/validation');
-
-const sessHost = config.get('sessionConfig.host');
-const sessPort = config.get('sessionConfig.port');
-const sessTtl = config.get('sessionConfig.ttl');
-const port = config.get('serverConfig.port');
-const host = config.get('serverConfig.host');
-
-require('express-reverse')(app);
-
-app.engine('hbs', expressHbs({
-  layoutsDir: 'views/layouts',
-  defaultLayout: 'layout',
-  extname: 'hbs',
-  helpers: {
-    equal: equalHelper.equal,
-  },
-}));
 
 const consumer = require('./consumer.js');
+const route = require('./route.js');
 
 consumer.start(io);
+route.start(app);
 
-app.use(express.static('public'));
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(session({
-  secret: 'keyboard cat',
-  cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 },
-  store: new RedisStore({
-    host: sessHost, port: sessPort, client, ttl: sessTtl,
-  }),
-  resave: false,
-  rolling: true,
-  saveUninitialized: true,
-}));
-
-app.use(cookieParser('secretSign#143_!223'));
-
-app.use(['/chat', '/chat/:id'], auth.isAuthenticated);
-
-app.get('register', '/register', authController.showRegisterForm);
-
-app.post('/register', validate.registration, authController.register);
-
-app.get('login', '/login', authController.showLoginForm);
-
-app.post('/login', validate.login, authController.login);
-
-app.get('logout', '/logout', authController.logout);
-
-app.get('/', auth.isAuthenticated, chatController.index);
-
-app.get('chat', '/chat', chatController.index);
-
-app.post(['/', '/chat'], validate.chatStore, chatController.create);
-
-app.delete('/chat/:id', chatController.delete);
-
-app.get('/chat/:id', messageController.index);
-
-app.post('/chat/:id', validate.messageStore, messageController.create);
-
-app.post('/chat-row-template', validate.chatTableRowData, chatController.renderTableRow);
-
-app.post('/message-row-template', validate.messageTableRowData, messageController.renderTableRow);
+const port = config.get('serverConfig.port');
+const host = config.get('serverConfig.host');
 
 http.listen(port, host, () => {});
---
+++ route.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const expressHbs = require('express-handlebars');
+const authController = require('./controllers/authController');
+const chatController = require('./controllers/chatController');
+const messageController = require('./controllers/messageController');
+const validate = require('./requests/validation');
+const auth = require('./middleware/auth');
+const equalHelper = require('./helpers/equalHelper');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+module.exports = {
+  start(app) {
+    require('express-reverse')(app);
+
+    app.engine('hbs', expressHbs({
+      layoutsDir: 'views/layouts',
+      defaultLayout: 'layout',
+      extname: 'hbs',
+      helpers: {
+        equal: equalHelper.equal,
+      },
+    }));
+
+    app.use(express.static('public'));
+
+    app.use(bodyParser.urlencoded({ extended: false }));
+    
+    app.use(cookieParser('secretSign#143_!223'));
+    
+    app.use(['/chat', '/chat/:id'], auth.isAuthenticated);
+
+    app.get('register', '/register', authController.showRegisterForm);
+
+    app.post('/register', validate.registration, authController.register);
+
+    app.get('login', '/login', authController.showLoginForm);
+
+    app.post('/login', validate.login, authController.login);
+
+    app.get('logout', '/logout', authController.logout);
+
+    app.get('/', auth.isAuthenticated, chatController.index);
+
+    app.get('chat', '/chat', chatController.index);
+
+    app.post(['/', '/chat'], validate.chatStore, chatController.create);
+
+    app.delete('/chat/:id', chatController.delete);
+
+    app.get('/chat/:id', messageController.index);
+
+    app.post('/chat/:id', validate.messageStore, messageController.create);
+
+    app.post('/chat-row-template', validate.chatTableRowData, chatController.renderTableRow);
+
+    app.post('/message-row-template', validate.messageTableRowData, messageController.renderTableRow);
+  },
+};
