9ceed235146a54c34f970e6f5778c79187623f27
Viktoriia Samokysh <viktoriia.samokysh@codeit.com.ua>
2019-07-17 12:36:49 +0300

    Let -> const change

--- migrations/20190702074533-drop-column-from-chats-table.js
+++ migrations/20190702074533-drop-column-from-chats-table.js
@@ -1,17 +1,11 @@
-
-
-let dbm;
-let type;
-let seed;
-
 /**
   * We receive the dbmigrate dependency from dbmigrate initially.
   * This enables us to not have to rely on NODE_PATH.
   */
 exports.setup = function (options, seedLink) {
-  dbm = options.dbmigrate;
-  type = dbm.dataType;
-  seed = seedLink;
+  const dbm = options.dbmigrate;
+  const type = dbm.dataType;
+  const seed = seedLink;
 };
 
 exports.up = function (db) {
